20be5a9fc2421cf2566dad99fdb4fe83a1459de2
Andrés Osorio <andresfelipe.osorio30@gmail.com>
2026-09-16 21:53:43 -0500

    context y provider del Auth

--- frontend/front-libreria/src/context/AuthContext.tsx
+++ frontend/front-libreria/src/context/AuthContext.tsx
@@ -1,9 +1,46 @@
 import { type User, users } from "../muck-users/user-mock.data";
-import {
-  useContext,
-  use,
-  useEffect,
-  useState,
-  type PropsWithChildren,
-  createContext,
-} from "react";
+import { createContext, useState, type PropsWithChildren } from "react";
+
+interface AuthContextProps {
+  currentUser: User | null;
+  login: (email: string, password: string) => boolean;
+  logout: () => void;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export const AuthContextProvider = ({ children }: PropsWithChildren) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (email: string, password: string): boolean => {
+    const user = users.find(
+      (user) => user.email === email && user.password === password,
+    );
+
+    if (!user) {
+      console.log("Credenciales incorrectas");
+      setUser(null);
+      return false;
+    }
+
+    setUser(user);
+    return true;
+  };
+
+  const handleLogout = () => {
+    console.log("logout");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext
+      value={{
+        currentUser: user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </AuthContext>
+  );
+};
